00441748315fbb71e322411e7c1e567059817644
Ramika De Silva <ramikadesilva0@gmail.com>
2025-10-25 03:34:56 -0700

    resize background to extension box

--- apps/extension/src/App.js
+++ apps/extension/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import BackgroundGlow from './components/BackgroundGlow.tsx';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <p>This is a React Chrome Extension</p>
-    </>
+    <BackgroundGlow>
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <p>This is a React Chrome Extension</p>
+    </BackgroundGlow>
   );
 }
 
